40f48338a4d0f74d67a653c25927e6dcc03d93d3
陈金鑫 <505150704@qq.com>
2018-08-03 21:06:58 +0800

    Completion of foundation construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ButtonDemo from './pageages/button/demo/index'
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p className="afo-vue-mobile-name">afo-vue-mobile</p>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+              <header className="App-header">
+                <p className="afo-vue-mobile-name">afo-vue-mobile</p>
+              </header>
+              <main>
+                <Route path="/button" component={ButtonDemo} />
+              </main>
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
---
+++ src/pageages/button/demo/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class ButtonDeom extends React.Component {
+  public render() {
+    return (
+      <div className="ButtonDeom">
+        <header className="App-header">
+          <p className="afo-vue-mobile-name">ButtonDeom</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default ButtonDeom;
