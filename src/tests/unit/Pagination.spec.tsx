587a8b0b4e07e18ca0515ec2eaefd2248e16681a
陈金鑫 <505150704@qq.com>
2018-08-23 19:50:08 +0800

    pagination test

---
+++ src/tests/unit/Pagination.spec.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import DemoTag from '../../pageages/pagination/demo';
+
+describe('Button', () => {
+  // 测试快照
+  it('test snapshots', () => {
+    const component = renderer.create(
+      <DemoTag />
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
